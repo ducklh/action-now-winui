dacc10ae06b3b8aa49eec45ef0dd8ac200c8c58b
leduc <hongduc@techzen.vn>
2025-04-23 00:02:12 +0700

    init

--- Views/DataGridPage.xaml.cs
+++ Views/DataGridPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using ActionNowWinUI.ViewModels;
-
 using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml;
+using System;
 
 namespace ActionNowWinUI.Views;
 
@@ -17,5 +18,60 @@ public sealed partial class DataGridPage : Page
     {
         ViewModel = App.GetService<DataGridViewModel>();
         InitializeComponent();
+    }
+
+    private void AddButton_Click(object sender, RoutedEventArgs e)
+    {
+        // TODO: Thêm logic để thêm dữ liệu mới
+        // Ví dụ: Mở dialog để nhập dữ liệu mới
+        ContentDialog addDialog = new ContentDialog
+        {
+            Title = "Thêm dữ liệu mới",
+            PrimaryButtonText = "Lưu",
+            CloseButtonText = "Hủy",
+            DefaultButton = ContentDialogButton.Primary,
+            XamlRoot = this.XamlRoot
+        };
+
+        // TODO: Thêm các control để nhập dữ liệu vào dialog
+        addDialog.ShowAsync();
+    }
+
+    private void DeleteButton_Click(object sender, RoutedEventArgs e)
+    {
+        if (DataGrid.SelectedItem != null)
+        {
+            // TODO: Thêm logic để xóa dữ liệu đã chọn
+            ContentDialog deleteDialog = new ContentDialog
+            {
+                Title = "Xác nhận xóa",
+                Content = "Bạn có chắc chắn muốn xóa mục này?",
+                PrimaryButtonText = "Xóa",
+                CloseButtonText = "Hủy",
+                DefaultButton = ContentDialogButton.Close,
+                XamlRoot = this.XamlRoot
+            };
+
+            deleteDialog.PrimaryButtonClick += async (s, args) =>
+            {
+                // TODO: Thêm logic xóa dữ liệu ở đây
+                // Ví dụ: ViewModel.DeleteItem(DataGrid.SelectedItem);
+            };
+
+            deleteDialog.ShowAsync();
+        }
+        else
+        {
+            ContentDialog noSelectionDialog = new ContentDialog
+            {
+                Title = "Thông báo",
+                Content = "Vui lòng chọn mục cần xóa",
+                CloseButtonText = "Đóng",
+                DefaultButton = ContentDialogButton.Close,
+                XamlRoot = this.XamlRoot
+            };
+
+            noSelectionDialog.ShowAsync();
+        }
     }
 }
